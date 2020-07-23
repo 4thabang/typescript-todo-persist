768226c8eb0d361de751d3dd30fa56433421012a
4thabang <58977471+4thabang@users.noreply.github.com>
2020-07-24 00:10:08 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import { render } from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(<App emptyMessage="" />, rootElement);
