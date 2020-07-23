e2b9700b0f0da2c8c686d35f9f8a71b08062cd76
4thabang <58977471+4thabang@users.noreply.github.com>
2020-07-23 23:59:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,75 @@
 import * as React from "react";
-import { v4 as uuidv4 } from "uuid";
 import "./styles.css";
 
-// Persistent State Function
-function usePersistentState(key: any, initVal: any) {
-  const [value, setValue] = React.useState(() => {
-    const perstVal = window.localStorage.getItem(key);
-    perstVal !== null ? JSON.parse(perstVal) : initVal;
-  });
-  React.useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+interface Reducer<R> {
+  (state: R, action: R): R;
 }
 
-interface PropsA {
-  (state: any, action: any): any;
-}
-
-enum Types {
-  ADD_TODO = "ADD_TODO",
-  DELETE_TODO = "DELETE_TODO"
-}
+type Typess = "ADD_TODO" | "TODO_COMPLETE" | "ALL_TODOS";
 
 // These are optional. They make the code cleaner
 // Ideally want to import these from another 'Types' folder.
-const ADD_TODO = Types.ADD_TODO;
-const DELETE_TODO = Types.DELETE_TODO;
+const ADD_TODO: Typess = "ADD_TODO";
+const TODO_COMPLETE: Typess = "TODO_COMPLETE";
 
 type Action =
   | { type: "ADD_TODO"; payload: string }
-  | { type: "DELETE_TODO"; payload: any };
+  | { type: "TODO_COMPLETE"; payload: any };
 
 const initialState = { todos: [] };
 type State = { todos: [] };
 
-let reducer: PropsA;
+let reducer: Reducer<any>;
 reducer = (state: State, action: Action) => {
   switch (action.type) {
     case ADD_TODO:
       return {
-        todos: [...state.todos, { id: uuidv4(), todos: action.payload }]
+        todos: [...state.todos, { id: 1, todos: action.payload }]
       };
-    case DELETE_TODO:
+    case TODO_COMPLETE:
       return state.todos.filter((todo: any) => todo.id !== state.todos);
     default:
       return state;
   }
 };
 
-const App: React.FC<{ message: string }> = ({ message = "" }) => {
+interface ISubmitTodo {
+  message: string | number | string[] | undefined;
+}
+
+// Persistent State Hook
+function usePersistentState(key: string, initVal: any) {
+  const [value, setValue] = React.useState(() => {
+    const perstVal = window.localStorage.getItem(key);
+    return perstVal !== null ? JSON.parse(perstVal) : initVal;
+  });
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}
+
+const App: React.FC<{ message: ISubmitTodo; emptyMessage: string }> = ({
+  message,
+  emptyMessage = ""
+}) => {
   const [{ todos }, dispatch] = React.useReducer(reducer, initialState);
 
-  const [text, setText] = React.useState(message);
+  const [text, setText] = usePersistentState("Todo", emptyMessage);
+
+  // const [text, setText] = React.useState(emptyMessage);
 
   const handleDispatch = (e: any) => {
     e.preventDefault();
-    if (text !== "") {
+    if (text !== emptyMessage) {
       dispatch({ type: ADD_TODO, payload: text });
-      setText(message);
+      setText(emptyMessage);
     }
   };
 
   const handleComplete = (e: any) => {
     e.preventDefault();
-    dispatch({ type: DELETE_TODO });
+    dispatch({ type: TODO_COMPLETE });
   };
 
   const handleText = (e: any) => setText(e.target.value);
@@ -77,8 +83,8 @@ const App: React.FC<{ message: string }> = ({ message = "" }) => {
       </form>
       {todos.map((todo: any) => (
         <React.Fragment key={todo.id}>
+          <input type="checkbox" onClick={handleComplete} />
           <p>{todo.todos}</p>
-          <button onClick={handleComplete}>Mark Complete</button>
         </React.Fragment>
       ))}
     </div>
