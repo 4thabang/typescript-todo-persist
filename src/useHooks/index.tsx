64035d06a53e5828ee3dee4f95708e3ebdf61dd0
4thabang <58977471+4thabang@users.noreply.github.com>
2020-07-29 00:39:03 +0100

    Update index.tsx

--- src/useHooks/index.tsx
+++ src/useHooks/index.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 export const usePersistentState = (key: any, initVal: any) => {
   const [value, setValue] = React.useState(() => {
     const perstVal = window.localStorage.getItem(key);
-    return perstVal !== null ? JSON.parse(perstVal) : initVal;
+     perstVal !== null ? JSON.parse(perstVal) : initVal;
   });
   React.useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(value));
