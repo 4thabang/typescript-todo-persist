475fe1126a01a23d86df292b88e8d073e25742e4
4thabang <58977471+4thabang@users.noreply.github.com>
2020-07-31 16:12:09 +0100

    Update index.tsx

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,5 +1,1 @@
-type Types = "ADD_TODO" | "TODO_COMPLETE" | "ALL_TODOS";
 
-export const ADD_TODO: Types = "ADD_TODO";
-export const TODO_COMPLETE: Types = "TODO_COMPLETE";
-export const ALL_TODOS: Types = "ALL_TODOS";
