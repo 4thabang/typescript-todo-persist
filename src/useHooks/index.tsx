9d54f77eaae3c50379ac8b882b01c3156b9d3284
4thabang <58977471+4thabang@users.noreply.github.com>
2021-12-01 14:35:28 +0000

    Update index.tsx

--- src/useHooks/index.tsx
+++ src/useHooks/index.tsx
@@ -1,21 +1,30 @@
 import * as React from "react";
 
+// Declare Reract hooks
+const { useState, useEffect } = React;
+
 type Persist = {
   key: string;
   initVal: any;
 };
 
 // Persistent State Hook
-export const usePersistentState = (
+const usePersistentState = (
   key: Persist["key"],
   initVal: Persist["initVal"]
 ) => {
-  const [value, setValue] = React.useState(() => {
-    const perstVal: string | null = window.localStorage.getItem(key);
-    return perstVal !== null ? JSON.parse(perstVal) : initVal;
-  });
-  React.useEffect(() => {
+  const persistentState = () => {
+    const valueKey: string | null = window.localStorage.getItem(key);
+    return valueKey != null ? JSON.parse(valueKey) : initVal;
+  };
+  
+  const [value, setValue] = useState<any>(persistentState);
+  
+  useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
+  
   return [value, setValue];
 };
+
+export { usePersistentState }
